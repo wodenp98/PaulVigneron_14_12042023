0b89b7dbfe201dc5ba5d556f18c7acc86b0f4802
Paul VIGNERON <paulvigneron4698@gmail.Com>
2023-04-27 19:20:42 +0200

    table responsive

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -76,77 +76,80 @@ const Table = () => {
     );
 
   return (
-    <div className="overflow-x-auto h-full">
-      <table
-        {...getTableProps()}
-        className="table-auto w-full border-collapse sm:table-responsive"
-      >
-        <thead className="bg-gray-200">
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  className="px-4 py-2 font-bold text-left cursor-pointer"
+    <div className="overflow-x-auto  w-full h-full flex-col sm:flex-row ">
+      <div className="sm:overflow-x-auto">
+        <table
+          {...getTableProps()}
+          className="table-auto w-full divide-y divide-gray-200"
+        >
+          <thead className="bg-gray-200">
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    className="px-4 py-2 font-bold text-left cursor-pointer text-sm md:text-base"
+                  >
+                    {column.render("Header")}
+                    <span className="ml-1">
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? " 🔽"
+                          : " 🔼"
+                        : ""}
+                    </span>
+                  </th>
+                ))}
+                <th className="px-4 py-2 font-bold text-left"></th>
+              </tr>
+            ))}
+          </thead>
+          <tbody
+            {...getTableBodyProps()}
+            className="bg-white divide-y divide-gray-200"
+          >
+            {rows.length === 0 ? (
+              <tr>
+                <td
+                  colSpan={headerGroups[0].headers.length + 1}
+                  className="text-center py-4"
                 >
-                  {column.render("Header")}
-                  <span className="ml-1">
-                    {column.isSorted
-                      ? column.isSortedDesc
-                        ? " 🔽"
-                        : " 🔼"
-                      : ""}
-                  </span>
-                </th>
-              ))}
-              <th className="px-4 py-2 font-bold text-left"></th>
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()} className="bg-white">
-          {rows.length === 0 ? (
-            <tr>
-              <td
-                colSpan={headerGroups[0].headers.length + 1}
-                className="text-center"
-              >
-                No data available
-              </td>
-            </tr>
-          ) : (
-            rows.map((row, rowIndex) => {
-              prepareRow(row);
-              return (
-                <tr
-                  {...row.getRowProps()}
-                  className={rowIndex % 2 === 0 ? "bg-gray-100" : ""}
-                >
-                  {row.cells.map((cell, cellIndex) => (
-                    <td
-                      {...cell.getCellProps()}
-                      className={`px-4 py-2 ${
-                        cellIndex === 0 ? "font-bold" : ""
-                      } sm:text-sm`}
-                    >
-                      {cell.render("Cell")}
+                  No data available
+                </td>
+              </tr>
+            ) : (
+              rows.map((row, rowIndex) => {
+                prepareRow(row);
+                return (
+                  <tr
+                    {...row.getRowProps()}
+                    className={rowIndex % 2 === 0 ? "bg-gray-100" : ""}
+                  >
+                    {row.cells.map((cell, cellIndex) => (
+                      <td
+                        {...cell.getCellProps()}
+                        className={`px-4 py-2 text-sm md:text-base`}
+                      >
+                        {cell.render("Cell")}
+                      </td>
+                    ))}
+                    <td className="px-4 py-2">
+                      <button
+                        onClick={() => handleDelete(row)}
+                        id="delete-employee"
+                        aria-label="Delete an employee"
+                        className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded text-sm md:text-base"
+                      >
+                        <FaTrash />
+                      </button>
                     </td>
-                  ))}
-                  <td className="px-4 py-2">
-                    <button
-                      onClick={() => handleDelete(row)}
-                      id="delete-employee"
-                      aria-label="Delete an employee"
-                      className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded sm:text-sm"
-                    >
-                      <FaTrash />
-                    </button>
-                  </td>
-                </tr>
-              );
-            })
-          )}
-        </tbody>
-      </table>
+                  </tr>
+                );
+              })
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
